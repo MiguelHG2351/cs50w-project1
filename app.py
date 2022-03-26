a0e8a3d017cac023850f6dbf5b10776007f5a744
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2022-03-25 22:13:10 -0600

    gunicorn add

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 # Check for environment variable
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
+if not os.getenv("URL_DATABASE"):
+    raise RuntimeError("URL_DATABASE is not set")
 
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
@@ -28,7 +28,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine(os.getenv("URL_DATABASE"))
 db = scoped_session(sessionmaker(bind=engine))
 
 def isAutenticated(f):
